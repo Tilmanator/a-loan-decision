f4020abd16db78d1bf4fb275c6128bf4d360541c
Tilman Lindig <tilman.lindig@rogers.com>
2018-06-07 23:46:08 -0400

    Update README, document run

--- README.md
+++ README.md
@@ -21,7 +21,6 @@ Current (handpicked) metrics:
 
 Achieving consistent 93% - 95% accuracy on test set, the Random Forest (mostly) sees employment length as the most important feature
 
-
 ## The Data
 Lending Club, 2018 Q1
 [Download files from here](https://www.lendingclub.com/info/download-data.action)
@@ -34,8 +33,28 @@ curl https://resources.lendingclub.com/LoanStats_2018Q1.csv.zip -o LoanStats_201
 curl https://resources.lendingclub.com/RejectStats_2018Q1.csv.zip -o RejectStats_2018Q1.csv.zip
 ```
 
+## Further setup
+From root directory,
+```bash
+make
+```
+This will install the required packages.
+You can now run
+```bash
+python run.py
+```
+or to see what arguments can be passed in:
+```bash
+python run.py -h
+```
+
 ## Testing
 Testing in test directory. Set up Travis in future, for now from root directory:
+```bash
+make test
+```
+or
+
 ```bash
 pytest
 ```
--- run.py
+++ run.py
@@ -9,16 +9,18 @@ def main(args):
     all_X, all_Y, column_names = data.get_data(get_feature_names=True)
     input_dim = len(all_X[0])
 
+    # Load the models
     nn = models.get_nn_model(input_dim)
     rf = models.get_random_forest_model()
 
+    # Create training set
     n_train = 1000
     X = all_X[:n_train]
     Y = all_Y[:n_train]
     # Fit the model
     nn.fit(X, Y, epochs=50, batch_size=10, verbose=2)
 
-    # Get some samples to test our model on
+    # Get some samples to test our model on, our test set
     test_set_range = (10000, 20000)
     x_test = all_X[test_set_range[0]:test_set_range[1]]
     y_test = all_Y[test_set_range[0]:test_set_range[1]]
@@ -35,6 +37,7 @@ def main(args):
 
     print "Random Forest Accuracy: %.4f%%" % (accuracy_score(y_test, rf_predictions))
 
+    # Importance as found by random forest model
     if args.feature_importance:
         print "\nRandom Forest Feature importance:"
         for i in zip(column_names, rf.feature_importances_):
@@ -43,6 +46,7 @@ def main(args):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Learn about loans!")
-    parser.add_argument('--feature-importance', '-fi', action='store_true')
+    parser.add_argument('--feature-importance', '-fi', action='store_true',
+                        help="Flag that displays feature importance when set")
     args, leftover = parser.parse_known_args()
     main(args)
